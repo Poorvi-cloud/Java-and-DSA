764d569cd9223dd847c12035e0bb41ff372d3c3e
Poorvi Sharma <poorvimmmindia@gmail.com>
2025-02-08 09:48:53 +0530

    Add files via upload

---
+++ Java and DSA/code_4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class code_4 {
+    public static void main(String[] args) {
+        //INPUT
+        // Scanner sc = new Scanner(System.in);
+        // //String name = sc.next();     
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // question to solve
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+
+    }
+}
